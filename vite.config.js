4fa58177d979c63ba8280838a6697b5c03bb11e4
Sorab Rajput <110166393+sorab0961@users.noreply.github.com>
2026-02-11 18:27:02 +0530

    Update base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-
-// https://vite.dev/config/
-export default defineConfig({
+export default {
   base: '/sorab-portfolio/',
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+  // other configurations
+};
